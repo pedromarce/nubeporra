b59c16d4775243d4fd0570db6a9c97f2695147cb
Pedro Marce Rodriguez <pedromarce@gmail.com>
2013-08-21 00:32:30 +0100

    Enhance home screen and more details

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   before_filter :authenticate_user!
   def home
-  	@games = Game.order('"gameTime"').where(:gameTime => 2.days.ago..10.days.from_now)
+  	@games = Game.order('"gameTime"').where(:gameTime => 5.days.ago..10.days.from_now)
   	@standings = User.all.sort_by { |user| user[:numpoints]}
   end
 end
