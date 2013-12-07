f7874591bc0484f9950f69525d8b52a75efae1f3
Pedro Marce Rodriguez <pedromarce@gmail.com>
2013-12-07 09:47:16 +0000

    Make scores api calls to work@

--- app/controllers/scoreapis_controller.rb
+++ app/controllers/scoreapis_controller.rb
@@ -13,7 +13,53 @@ class ScoreapisController < ApplicationController
     end
   end
 
+  def footballnext
+    api = Scoreapi.new
+    time = Time.parse(params[:date]) + 1.days
+    games = api.football(time.strftime('%Y-%m-%d'))
+    @games = games.parsed_response['matches'] if games.parsed_response['matches']
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @games }
+    end
+  end
+
+  def footballprev
+    api = Scoreapi.new
+    time = Time.parse(params[:date]) - 1.days
+    games = api.football(time.strftime('%Y-%m-%d'))
+    @games = games.parsed_response['matches'] if games.parsed_response['matches']
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @games }
+    end
+  end
+
   def nba
+    api = Scoreapi.new
+    @games = []
+    games = api.nba(params[:date])
+    @games = games.parsed_response['Schedule']['League']['Game'] if games.parsed_response['Schedule']['League']['Game']
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @games }
+    end
+  end
+
+  def nbanext
+    api = Scoreapi.new
+    @games = []
+    games = api.nba(params[:date])
+    @games = games.parsed_response['Schedule']['League']['Game'] if games.parsed_response['Schedule']['League']['Game']
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @games }
+    end
+  end
+
+  def nbaprev
     api = Scoreapi.new
     @games = []
     games = api.nba(params[:date])
