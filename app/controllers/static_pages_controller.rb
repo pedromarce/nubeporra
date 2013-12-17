e97c33b833d24a456be805de83187b122ec78e68
Pedro Marce Rodriguez <pedromarce@gmail.com>
2013-12-17 18:11:19 +0000

    Change games visible based on close time

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   before_filter :authenticate_user!
   def home
-  	@games = Game.order('"gameTime"').where(:gameTime => 5.days.ago..10.days.from_now).includes(:bets => {:game => :bets})
+  	@games = Game.order('"gameTime"').where(:closeTime => 5.days.ago..10.days.from_now).includes(:bets => {:game => :bets})
   	@standings = User.all.includes(:bets => {:game => :bets}).sort_by { |user| user.numpoints * -1 }
   end
 end
